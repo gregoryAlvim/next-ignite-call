0a37ba22df30ea61aa80882d3f39584545c95c86
Grégory Alvim <gregori.jagua@gmail.com>
2024-02-19 21:36:20 -0300

    create register pages

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,103 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Container, Form, FormError, Header } from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { api } from '@/src/lib/axios'
+import { AxiosError } from 'axios'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'No mínimo 3 caracteres!' })
+    .regex(/^([a-z\\-]+)$/i, { message: 'Só é permitido letras e hifens!' })
+    .transform((username) => username.toLowerCase()),
+  name: z.string().min(3, { message: 'No mínimo 3 caracteres!' }),
+})
+
+type registerFormData = z.infer<typeof registerFormSchema>
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<registerFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query.username, setValue])
+
+  async function handleClaimUsername(data: registerFormData) {
+    try {
+      const { name, username } = data
+
+      await api.post('/users', {
+        name,
+        username,
+      })
+
+      await router.push('/register/connect-calendar')
+    } catch (error) {
+      if (error instanceof AxiosError && error?.response?.data.message) {
+        alert(error?.response.data.message)
+      }
+
+      console.error(error)
+    }
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form onSubmit={handleSubmit(handleClaimUsername)} as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu-usuário"
+            {...register('username')}
+          />
+
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
+        </label>
+
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" {...register('name')} />
+
+          {errors.name && (
+            <FormError size="sm">{errors.name.message}</FormError>
+          )}
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
